fe5c968128a42c05e9c639b27d6a8a841ac113af
Andrey Mamchenko <atmamont@gmail.com>
2016-01-29 23:06:03 +0200

    Initial Commit

---
+++ PhotoPrintUA/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhotoPrintUA
+//
+//  Created by atMamont on 29.01.16.
+//  Copyright © 2016 Andrey Mamchenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
